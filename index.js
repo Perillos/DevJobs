165d4e48b758e4dc9939453160fc4e8ba15d0297
fvalero <fvalero@gesio.com>
2023-10-25 17:56:36 +0200

    feat: add config router and views

--- index.js
+++ index.js
@@ -1,10 +1,33 @@
 const express = require("express");
+const exphbs = require('express-handlebars');
+const path = require('path')
+const router = require('./routes')
+
+
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5050;
 
-app.use("/", (req, res) => {
-  res.send("Funciona");
-});
 
-app.listen(PORT);
+// app.use('/', (req, res) => {
+//   res.send('Funciona en Index');
+// })
+
+// habilitar handlebars como view
+app.engine('handlebars', 
+  exphbs.engine({
+    defaultLayout: 'layout',
+  })
+)
+
+app.set('view engine', 'handlebars');
+
+// static file
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use("/", router());
+
+// Arranque del proyecto
+app.listen(PORT, () => {
+  console.log(`El Servidor esta funcionando en el puero http://localhost:${PORT}`);
+})
